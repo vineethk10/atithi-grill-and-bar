f69e46da09b1296f62436a2ce484ff1bffb054f2
Vineeth Kumar Ananthula <vineethkumarananthula@gmail.com>
2024-07-05 17:15:45 -0500

    Updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ 
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'contact', component: ContactComponent }];
+
+
+
+
