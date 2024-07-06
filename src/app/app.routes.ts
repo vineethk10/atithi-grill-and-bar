6ce4a4eae44336cfff90296786c09fed91df0b3c
Vineeth Kumar Ananthula <vineethkumarananthula@gmail.com>
2024-07-05 21:07:57 -0500

    Updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { MenuComponent } from './menu/menu.component';
-import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [ 
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
     { path: 'menu', component: MenuComponent },
-    { path: 'contact', component: ContactComponent }];
+    { path: '**', redirectTo: '' }];
 
 
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +10,26 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  isNavbarCollapsed = true;
+
+  constructor(private location: Location, private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.setActiveLink();
+  }
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  setActiveLink() {
+    const links = this.el.nativeElement.querySelectorAll('.nav-link');
+    links.forEach((link: HTMLElement) => {
+      this.renderer.removeClass(link, 'active');
+      if (link.getAttribute('href') === this.location.path()) {
+        this.renderer.addClass(link, 'active');
+      }
+    });
+  }
+}
